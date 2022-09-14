1a4e6de8098b650dadffac0128f6ae55d6be9766
Luthfi A Pratama <luthfianump@gmail.com>
2022-09-14 13:11:08 +0700

    init custom section

---
+++ custom_sections/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from product.models import Product
+
+# Create your models here.
+class CustomSection(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product, blank=True)
+
+    
