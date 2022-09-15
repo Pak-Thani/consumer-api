24e7d195b063196a1602ac3315b19edd66a951d5
RaidyMR <raidymustafid39@gmail.com>
2022-09-15 22:14:32 +0700

    update models requirement

--- product/models.py
+++ product/models.py
@@ -2,9 +2,11 @@ from email.policy import default
 from django.db import models
 
 class Product(models.Model):
-    name =  models.CharField(max_length=30)
-    description = models.CharField(max_length=150)
-    image = models.CharField(max_length=150)
-    slug = models.CharField(max_length=30)
-    is_available = models.BooleanField()
+    name =  models.CharField(max_length=32)
+    description = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    slug = models.CharField(max_length=8)
+    qty = models.CharField(max_length=8)
+    stockAvailable = models.PositiveIntegerField()
+    isStockAvailable = models.BooleanField()
     
