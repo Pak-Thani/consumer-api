fb32c8c25ebcdbf33f3a549ce5bcb1655e46ce72
Luthfi A Pratama <luthfianump@gmail.com>
2022-09-16 07:55:26 +0700

    add is_active field in customsections

--- custom_sections/models.py
+++ custom_sections/models.py
@@ -5,6 +5,7 @@ from product.models import Product
 class CustomSection(models.Model):
     title = models.CharField(max_length=100)
     slug = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
     products = models.ManyToManyField(Product, blank=True)
 
     def onlyGetSixProducts(self):
