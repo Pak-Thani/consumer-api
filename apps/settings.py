609d5c5e9ea77af51b38078f82c6a274dac41ca1
shiroul <mjamiulakbar@gmail.com>
2022-09-22 16:21:00 +0700

    change setting

--- apps/settings.py
+++ apps/settings.py
@@ -146,13 +146,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = {
-    os.path.join(BASE_DIR, 'static_project')
-}
-
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", 'static_root')
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
