88c0df871f9c8ed759f676d0c74e48532c339ef3
RaidyMR <raidymustafid39@gmail.com>
2022-09-23 12:08:25 +0700

    modify product models

--- product/models.py
+++ product/models.py
@@ -1,13 +1,17 @@
-from email.policy import default
 from django.db import models
+from category.models import Category
 
 class Product(models.Model):
     name =  models.CharField(max_length=32)
     description = models.CharField(max_length=255)
     image = models.CharField(max_length=255)
-    slug = models.CharField(max_length=8)
-    qty = models.CharField(max_length=8)
+    slug = models.CharField(max_length=32)
+    qty = models.CharField(max_length=32)
     pricePerQty = models.PositiveIntegerField()
     stockAvailable = models.PositiveIntegerField()
     isStockAvailable = models.BooleanField()
-    
+    category = models.ForeignKey(Category, related_name='products', blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
