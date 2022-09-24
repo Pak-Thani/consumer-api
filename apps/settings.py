07184224635cffc5df9c7007c0f73bce5e95e0db
shiroul <mjamiulakbar@gmail.com>
2022-09-25 02:53:37 +0700

    add product in setting

--- apps/settings.py
+++ apps/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'storages',
+    'product',
 ]
 
 REST_FRAMEWORK = {
@@ -144,8 +146,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles-cdn"
+
+from .cdn.conf import *
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
