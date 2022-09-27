1540895ab509fce472066e769edf42ae08c3cf78
shiroul <mjamiulakbar@gmail.com>
2022-09-27 19:45:38 +0700

    edit product.admin

--- product/models.py
+++ product/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     name =  models.CharField(max_length=32)
     description = models.CharField(max_length=255)
-    image = models.CharField(max_length=255)
+    image = models.FileField(upload_to='product/')
     slug = models.CharField(max_length=8)
     qty = models.CharField(max_length=8)
     pricePerQty = models.PositiveIntegerField()
