13249f634fc9cd59837fd4aef28691986ccb5c08
Luthfi A Pratama <luthfianump@gmail.com>
2022-10-03 23:27:34 +0700

    add list product search view

--- product/views.py
+++ product/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
-
+from rest_framework import generics
 from product.models import Product
 from product.serializers import ProductSerializer
+from product.paginations import CustomPagination
 from apps.custom_response import CustomResponse
 
 class ProductView(APIView):
@@ -11,5 +12,23 @@ class ProductView(APIView):
             serializer = ProductSerializer(product_item)
             return CustomResponse.success(serializer.data)
 
+        except Product.DoesNotExist:
+            return CustomResponse.notFound(error='Product Not Found')
+
+class SearchProductView(generics.ListAPIView):
+    pagination_class = CustomPagination
+    serializer_class = ProductSerializer
+
+    def get(self, request, keyword):
+        try:
+            products = Product.objects.filter(name__icontains = keyword)
+            serializer = self.serializer_class(products, many=True)
+
+            page = self.paginate_queryset(products)
+            if page is not None:
+                serializer = self.serializer_class(page, many=True)
+                data = self.get_paginated_response(serializer.data)
+                return CustomResponse.success(data)
+                
         except Product.DoesNotExist:
             return CustomResponse.notFound(error='Product Not Found')
