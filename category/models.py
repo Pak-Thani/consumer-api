2fcfaa8bda66497fb9b65515e1c008f9aadab68e
Luthfi A. Pratama <luthfianump@gmail.com>
2022-10-17 13:19:07 +0700

    add icon category cdn

--- category/models.py
+++ category/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
   name = models.CharField(max_length=32)
-  icon = models.CharField(max_length=255)
+  icon = models.FileField(upload_to='icon-category/')
   slug = models.CharField(max_length=32)
 
   def __str__(self):
