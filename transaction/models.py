6d44c3085167163e166af8f965fdcb067ae467a1
RaidyMR <raidymustafid39@gmail.com>
2022-10-27 18:44:23 +0700

    add required field

--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+    nama = models.CharField(max_length=32)
+    kabupaten = models.CharField(max_length=32)
+    kecamatan = models.CharField(max_length=32)
+    alamat_detail = models.CharField(max_length=32)
+    alamat = models.CharField(max_length=32)
+    nomor_telepon = models.CharField(max_length=16)
+    pembayaran = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.nama
