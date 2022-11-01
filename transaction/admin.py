8ddeb5ebb31e0fa69cfe7d1af5824c5c54a1ce34
RaidyMR <raidymustafid39@gmail.com>
2022-11-01 15:41:43 +0700

    add admin access to transaction models

--- transaction/admin.py
+++ transaction/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Transaction
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('created', 'status', 'payment')
+
+admin.site.register(Transaction, TransactionAdmin)
