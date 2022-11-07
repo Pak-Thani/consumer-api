0cf86738bfe30cd2d5a7782f48f1472b52ab4098
RaidyMR <raidymustafid39@gmail.com>
2022-11-07 15:21:14 +0700

    change payment to pembayaran

--- transaction/admin.py
+++ transaction/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Transaction
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('created', 'status', 'payment')
+    list_display = ('created', 'status', 'pembayaran')
 
 admin.site.register(Transaction, TransactionAdmin)
