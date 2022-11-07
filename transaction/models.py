2a4cbed9d78813d8fa043c54b115ee30a4aa4619
RaidyMR <raidymustafid39@gmail.com>
2022-11-07 11:13:05 +0700

    modify transaction model

--- transaction/models.py
+++ transaction/models.py
@@ -1,15 +1,26 @@
 from django.db import models
+from product.models import Product
 
 class Transaction(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=32)
+
+    #customer data
+    namaPembeli = models.CharField(max_length=32)
+    nomorWhatsapp = models.CharField(max_length=16)
+
+    #address
     kabupaten = models.CharField(max_length=32)
     kecamatan = models.CharField(max_length=32)
-    addressDetail = models.CharField(max_length=32)
-    address = models.CharField(max_length=32)
-    whatsappNumber = models.CharField(max_length=16)
-    payment = models.CharField(max_length=32)
-    status = models.CharField(max_length=32)
+    detailAlamat = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=100)
+
+    #cart
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.IntegerField(default=1)
+    total = models.IntegerField()
+    
+    pembayaran = models.CharField(max_length=32)
+    status = models.CharField(max_length=32, default="Belum Dibayar")
 
     def __str__(self):
-        return self.name
+        return self.namaPembeli
