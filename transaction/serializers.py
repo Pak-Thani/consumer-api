a1ed6d725539771e5ded9d112b055464604cb951
RaidyMR <raidymustafid39@gmail.com>
2022-11-08 15:54:11 +0700

    initiate TransactionProductSerializer

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 from product.serializers import ProductSerializer
 from .models import Transaction, TransactionProduct
-class TransactionSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(many=True, read_only=True)
-    
-
-    class Meta:
-        model = Transaction
-        fields = ('id', 'created', 'namaPembeli', 'nomorWhatsapp', 'kabupaten', 'kecamatan', 'detailAlamat', 'alamat', 'products', 'total', 'pembayaran', 'status')
 
 class TransactionProductSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
     class Meta:
         model = TransactionProduct
         fields = ('id', 'product', 'quantity')
+        
+class TransactionSerializer(serializers.ModelSerializer):
+   products = TransactionProductSerializer(many=True, read_only=True)
+   class Meta:
+        model = Transaction
+        fields = ('id', 'created', 'namaPembeli', 'nomorWhatsapp', 'kabupaten', 'kecamatan', 'detailAlamat', 'alamat', 'products', 'total', 'pembayaran', 'status')
