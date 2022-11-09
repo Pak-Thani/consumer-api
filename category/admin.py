dc05232d3ef5b437a6197512e1c081c69bd96f01
RaidyMR <raidymustafid39@gmail.com>
2022-11-09 20:57:10 +0700

    create read only slug in category

--- category/admin.py
+++ category/admin.py
@@ -4,5 +4,6 @@ from .models import Category
 
 class CategoryAdmin(admin.ModelAdmin):
   list_display = ('name', 'slug')
+  readonly_fields = ('slug',)
 
 admin.site.register(Category, CategoryAdmin)
