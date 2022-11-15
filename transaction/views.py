d871bd67eb39ece6fd9a4f2553967c77aa51a95b
RaidyMR <raidymustafid39@gmail.com>
2022-11-15 12:09:03 +0700

    clean up transaction views

--- transaction/views.py
+++ transaction/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from apps.custom_response import CustomResponse
-from .models import Transaction
 from .serializers import TransactionSerializer
 from rest_framework.views import APIView
 
@@ -12,16 +11,3 @@ class TransactionView(APIView):
             return CustomResponse.success(serializer.data)
 
         return CustomResponse.badRequest(serializer.errors)
-
-    # def put(self, request, id):
-        # try:
-        #     transaction = Transaction.objects.get(id=id)
-        #     serializer = TransactionSerializer(transaction, data=request.data)
-        #     if serializer.is_valid():
-        #         serializer.save()
-        #         return CustomResponse.success(serializer.data)
-
-        #     return CustomResponse.error(serializer.errors, 400)
-
-        # except Transaction.DoesNotExist:
-        #     return CustomResponse.notFound(error='Transaction not found')
