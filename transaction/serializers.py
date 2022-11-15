338fb34e9bfc23b7577f5c93565ff0f1064728e2
RaidyMR <raidymustafid39@gmail.com>
2022-11-15 12:03:28 +0700

    modify transaction serializers

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,15 +1,23 @@
 from rest_framework import serializers
 from product.serializers import ProductSerializer
 from .models import Transaction, TransactionProduct
+from drf_writable_nested import WritableNestedModelSerializer
 
 class TransactionProductSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
+    product = ProductSerializer(read_only=True)
     class Meta:
         model = TransactionProduct
-        fields = ('id', 'product', 'quantity')
+        fields = ('id', 'product', 'transaction', 'quantity')
         
-class TransactionSerializer(serializers.ModelSerializer):
-   products = TransactionProductSerializer(many=True, read_only=True)
-   class Meta:
+class TransactionSerializer(WritableNestedModelSerializer):
+    products = TransactionProductSerializer(many=True)
+    class Meta:
         model = Transaction
         fields = ('id', 'created', 'namaPembeli', 'nomorWhatsapp', 'kabupaten', 'kecamatan', 'detailAlamat', 'alamat', 'products', 'total', 'pembayaran', 'status')
+
+    def create(self, validated_data):
+        products_data = validated_data.pop('products')
+        transaction = Transaction.objects.create(**validated_data)
+        for product_data in products_data:
+            TransactionProduct.objects.create(transaction=transaction, **product_data)
+        return transaction
