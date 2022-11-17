ccbb2efc25ca8b4d7125aee04abd5b9e8e0901c4
RaidyMR <raidymustafid39@gmail.com>
2022-11-17 18:05:37 +0700

    modify transaction serializers

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,23 +1,33 @@
 from rest_framework import serializers
+from .models import Transaction, TransactionProduct, Product
 from product.serializers import ProductSerializer
-from .models import Transaction, TransactionProduct
-from drf_writable_nested import WritableNestedModelSerializer
+
+
 
 class TransactionProductSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(read_only=True)
+    id = serializers.ReadOnlyField(source='product.id')
+    product = serializers.ReadOnlyField(source='product.name')
     class Meta:
         model = TransactionProduct
-        fields = ('id', 'product', 'transaction', 'quantity')
-        
-class TransactionSerializer(WritableNestedModelSerializer):
-    products = TransactionProductSerializer(many=True)
+        fields = ('id', 'product', 'product_slug', 'quantity')
+
+class TransactionSerializer(serializers.ModelSerializer):
+    products = TransactionProductSerializer(source="transactionproduct_set", many=True)
     class Meta:
         model = Transaction
-        fields = ('id', 'created', 'namaPembeli', 'nomorWhatsapp', 'kabupaten', 'kecamatan', 'detailAlamat', 'alamat', 'products', 'total', 'pembayaran', 'status')
+        fields = '__all__'
+        depth = 2
 
     def create(self, validated_data):
-        products_data = validated_data.pop('products')
+        products_data = validated_data.pop('transactionproduct_set')
+        print(validated_data)
         transaction = Transaction.objects.create(**validated_data)
         for product_data in products_data:
-            TransactionProduct.objects.create(transaction=transaction, **product_data)
+            print(product_data)
+            TransactionProduct.objects.create(
+                transaction=transaction, 
+                product=Product.objects.get(slug=product_data['product_slug']),
+                product_slug=product_data['product_slug'],
+                quantity=product_data['quantity']
+            )
         return transaction
