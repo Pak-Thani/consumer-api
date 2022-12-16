02aacf0aece02b12c984c517b020eafd3d899637
RaidyMR <raidymustafid39@gmail.com>
2022-12-16 21:36:33 +0700

    create get and put method

--- transaction/views.py
+++ transaction/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from apps.custom_response import CustomResponse
+from .models import Transaction
 from .serializers import TransactionSerializer
 from rest_framework.views import APIView
 
@@ -11,3 +11,24 @@ class TransactionView(APIView):
             return CustomResponse.success(serializer.data)
 
         return CustomResponse.badRequest(serializer.errors)
+
+    def get(self, request, id):
+        try:
+            transaction = Transaction.objects.get(id = id)
+            serializer = TransactionSerializer(transaction)
+            return CustomResponse.success(serializer.data)
+
+        except Transaction.DoesNotExist:
+            return CustomResponse.notFound(error='Transaction Not Found')
+
+    def put(self, request, id):
+        try:
+            transaction = Transaction.objects.get(id = id)
+            transaction.buktiPembayaran = request.data['buktiPembayaran']
+            transaction.save()    
+
+            serializer = TransactionSerializer(transaction)
+            return CustomResponse.success(serializer.data)
+
+        except Transaction.DoesNotExist:
+            return CustomResponse.notFound(error='Transaction Not Found')
